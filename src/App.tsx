3a5e378f3e46f1fa40437940c85a6239dcdfa9fa
Scott O'Malley <scottyomalley@gmail.com>
2018-06-29 19:15:05 +0100

    Make the login responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { Button, createStyles, Grid, Paper, TextField, Theme, WithStyles, withStyles } from '@material-ui/core';
+import { Button, createStyles, Grid, Paper, TextField, Theme, Typography, WithStyles, withStyles } from '@material-ui/core';
 import CSSBaseline from '@material-ui/core/CssBaseline';
 import * as React from 'react';
 import './App.css';
 
 const styles = (theme: Theme) => createStyles({
-  buttonContainer:{
+  buttonContainer: {
     flexGrow: 1,
     'margin-top': '48px'
   },
   paper: {
-    'margin': '0 auto',
-    'max-width': '640px',
-    'padding': '48px 48px 32px',
-    'position': 'relative',
-    'top': '100px',
+    [theme.breakpoints.up("sm")]: {
+      'margin': '0 auto',
+      'max-width': '640px',
+      'padding': '48px 48px 32px',
+      'position': 'relative',
+      'top': '100px',
+    },
+    [theme.breakpoints.down("xs")]: {
+      "border-radius": 0,
+      "margin": 0,
+      "min-height": "100%",
+      "padding": " 48px 16px 16px",
+      'top': 0,
+    }
   },
   textField: {
     'width': '100%'
@@ -28,10 +37,10 @@ class App extends React.Component<Props> {
     return (
       <div>
         <CSSBaseline />
-
-
-
         <Paper elevation={14} className={classes.paper}>
+          <Typography variant="display1" gutterBottom={true} align="center">
+            Fencing Ireland Membership
+          </Typography>
           <TextField
             id="email"
             label="Email"
@@ -46,7 +55,7 @@ class App extends React.Component<Props> {
             type="password"
           />
           <Grid container={true} justify="space-between" className={classes.buttonContainer} >
-            <Grid item={true}><Button>Forgot Password</Button></Grid>
+            <Grid item={true}><Button variant="flat">Forgot Password</Button></Grid>
             <Grid item={true}><Button variant="contained" color="primary">Login</Button></Grid>
           </Grid>
 
