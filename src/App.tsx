cd71a8b1ee90511d1abc17ae9528f1f6cf44b98a
Clodagh ML <coder@scamallach.com>
2018-07-24 22:13:17 +0100

    WIP with problem passing isAuthenticated via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { createStyles, CssBaseline, Theme, WithStyles, withStyles } from '@material-ui/core';
 import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Checkout from './components/Checkout';
 import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 import SignUp from './components/SignUp';
+import firebase from './helpers/firebase';
 
 const styles = (theme: Theme) => createStyles({
   wrapper: {
@@ -19,7 +16,23 @@ const styles = (theme: Theme) => createStyles({
 
 interface Props extends WithStyles<typeof styles> { }
 
-class App extends React.Component<Props> {
+class App extends React.Component<Props, { isLoggedIn: boolean }> {
+
+  constructor(props: Props) {
+    super(props)
+    this.state = { isLoggedIn: false }
+  }
+
+  public componentWillMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      const isLoggedIn = user ? true : false;
+      this.setState({ isLoggedIn })
+
+    })
+  }
+
+  public login = (props: any) => <Login {...props} />
+
   public render() {
     const { classes } = this.props;
     return (
@@ -27,9 +40,9 @@ class App extends React.Component<Props> {
         <CssBaseline />
         <Router>
           <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/checkout" component={Checkout} />
+            <Route path="/login" render={this.login(props)} isAuthenticated={this.state.isLoggedIn} />
+            <Route path="/signup" component={ SignUp }/>
+            <PrivateRoute path="/checkout" component={Checkout} isAuthenticated={this.state.isLoggedIn} />
             <Redirect exact={true} to='/login' />
           </Switch>
         </Router>
