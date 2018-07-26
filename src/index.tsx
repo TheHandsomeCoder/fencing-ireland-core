fc4511b9f8e4d6d0a491a8076d42217c4335e6bd
Scott O'Malley <scottyomalley@gmail.com>
2018-07-26 23:57:45 +0100

    Added Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { EnthusiasmAction } from './actions';
 import App from './App';
 import './index.css';
+import { enthusiasm } from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, { enthusiasmLevel: 1, languageName: 'TypeScript' });
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App/></Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
