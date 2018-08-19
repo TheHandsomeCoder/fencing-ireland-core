3498536af2d4e98792ea52f4a313fb175f487ae3
Scott O'Malley <scottyomalley@gmail.com>
2018-08-20 00:44:10 +0100

    Fixed Signup, added history

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import './index.css';
-import store, { history } from './redux/store';
+import store from './redux/store';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App history={history}/>
+    <App/>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
